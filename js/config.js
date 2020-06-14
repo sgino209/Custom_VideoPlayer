c3def010f0f964a28fdd99e9449dbce631546b46
Shahar Gino <sgino@apple.com>
2020-06-14 08:41:37 +0300

    initial release

---
+++ js/config.js
@@ -0,0 +1,43 @@
+/**
+ * @file Manages the configuration settings for the Custom Video Player.
+ * @author Jay Kapoor <jatinkapoor995@gmail.com>
+ * @version 0.0.1 
+ * All rights reserved to Shahar Gino, June 2020
+ */
+
+var overall_duartion='00:09:00';
+var marker_position='00:01:00:';
+var marker_width='00:05:00:';
+var sources = {
+
+		source1:{
+
+			name:"media/source1.mp4",
+			start:'00:00:35',
+			end:'00:01:10',
+			tooltip:'tooltip1'
+
+		},
+		source2:{
+
+			name:"media/source2.mp4",
+			start:'00:02:10',
+			end:'00:03:20',
+			tooltip:'tooltip2'
+
+		},source3:{
+
+			name:"media/audio1.ogg",
+			start:'00:04:45',
+			end:'00:06:00',
+			tooltip:'tooltip3',
+
+		},source4:{
+
+			name:"media/audio2.m4a",
+			start:'00:06:50',
+			end:'00:08:00',
+			tooltip:'tooltip4'
+		}
+	};
+
